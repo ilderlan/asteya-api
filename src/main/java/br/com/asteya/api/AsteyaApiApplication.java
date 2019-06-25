3d3aea66f7ce9f174581e18fe2c803d582d8c64d
Ilderlan Costa Pinho <ilderlan.pinho@gmail.com>
2019-06-24 23:41:22 -0300

    Primeiro commit, asteya controle financeiro API.

---
+++ src/main/java/br/com/asteya/api/AsteyaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.asteya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteyaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsteyaApiApplication.class, args);
+    }
+
+}
