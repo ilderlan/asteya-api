8f4f5fcee04712bb8acf9c612f32311c41606f37
Ilderlan Costa Pinho <ilderlan.pinho@gmail.com>
2019-06-25 23:50:19 -0300

    Modelo Tag.

--- src/main/java/br/com/asteya/api/tags/Tag.java
+++ src/main/java/br/com/asteya/api/tags/Tag.java
@@ -3,14 +3,15 @@ package br.com.asteya.api.tags;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode(of = "codigo")
 @Entity
 @Table(name = "tag")
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
     private String nome;
 }
